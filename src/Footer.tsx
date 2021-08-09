734b4871a8f88c3885abf765320efc1f47ca0047
PlaneCargo191 <t.james_17@hotmail.com>
2021-08-09 22:40:16 +0700

    DONE

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <p className='text-center text-gray-400'> Copyright © 2021 </p>
+        <p className='text-center text-gray-400'>Copyright © 2021</p>
     )
 }
 
